74f465f31abbc256ad3e4ed826c58c089233bb7c
cr92 <agni_18_roy@yahoo.com>
2017-03-07 14:30:41 +0530

    string parsing added

--- apis/apis.js
+++ apis/apis.js
@@ -5,7 +5,15 @@ var config = require('../config.js');
 var connection = mysql.createConnection(config.db_config);
 connection.connect();
 
-function updatePosition(data, callback) {}
+// TEST RUNS ------>
+var str = '1473997674.817611 : 8=FIX.4.2|35=8|39=2|44=1340|48=54572|38=1000|151=0|32=1000|31=1340|11=110000072|37=1100000000081761|40=0|17=16673431|6=19644600|14=0|54=2|55=CIPLA|20=0|150=2|60=20160916-03:47:54|10=000|';
+updatePosition(str);
+// TEST RUNS <------
+
+function updatePosition(data, callback) {
+  var processed_data = (parseInput(data));
+
+}
 
 function updateTrades(data, callback) {}
 
@@ -34,7 +42,14 @@ function fetchAllPositions(data, callback) {
   });
 }
 
-function parseInput(data, callback) {}
+function parseInput(data) {
+  var split_array = data.split('|');
+  var processed_data = {};
+  processed_data.instrument_id = split_array[4].split('=')[1];
+  processed_data.trade_volume = split_array[7].split('=')[1];
+  processed_data.trade_type = split_array[15].split('=')[1];
+  return processed_data;
+}
 
 module.exports = {
   updatePosition: updatePosition,
