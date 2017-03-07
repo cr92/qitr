afa8282a35b8bcd5204a058e679ed731196df59b
cr92 <agni_18_roy@yahoo.com>
2017-03-08 04:50:41 +0530

    autorefresh every 10s

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,18 @@
 var app = angular.module('index_show_all_positions', []);
-app.controller('index_all_positions', function ($scope, $http) {
-  $http({
-    method: 'GET',
-    url: 'showAll'
-  }).then(function Succes(response) {
-    console.log(response);
-    $scope.positions = response.data;
-  }, function Error(response) {
-    console.log(response);
-    $scope.all_data = response.statusText;
-  });
+app.controller('index_all_positions', function ($scope, $http, $timeout) {
+  $scope.requestData = function () {
+    $http({
+      method: 'GET',
+      url: 'showAll'
+    }).then(function Succes(response) {
+      console.log(response);
+      $scope.positions = response.data;
+    }, function Error(response) {
+      console.log(response);
+      $scope.all_data = response.statusText;
+    });
+  };
+
+  $scope.requestData();
+  setInterval($scope.requestData, 10000);
 });
