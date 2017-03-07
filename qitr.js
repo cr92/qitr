ea1a9f4f3d8423819d94654b1e34ef2bca89825f
cr92 <agni_18_roy@yahoo.com>
2017-03-07 12:18:05 +0530

    express server added

---
+++ qitr.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var http = require("http");
+var path = require("path");
+var bodyParser = require("body-parser");
+var apis=require('./apis/apis');
+var app = express();
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function (request, response) {
+  console.log('boo-hoo');
+});
+
+app.get('/showAll', function (request, response) {
+  console.log('hoo-hoo');
+});
+
+app.get('/instrument/*', function (request, response) {
+  console.log('yoo-hoo');
+});
+
+var server = app.listen(9090, function () {
+  var port = server.address().port;
+  console.log("listening at port: %s",port)
+});
