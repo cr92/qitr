6ce6f8773eb9edc05ce43e11e00fd0b1451e23d6
cr92 <agni_18_roy@yahoo.com>
2017-03-08 10:47:11 +0530

    trade string processed on button click and clears on successful insertion

--- public/js/enter.js
+++ public/js/enter.js
@@ -1,20 +1,23 @@
 var app = angular.module('enter_trade_data', []);
 app.controller('post_trade_data', function ($scope, $http) {
-    var datax = {
-        data_string: '1473997674.817611 : 8=FIX.4.2|35=8|39=2|44=1340|48=54552|38=1000|151=0|32=1000|31=1340|11=110000072|37=1100000000081761|40=0|17=16673431|6=19644600|14=0|54=2|55=CIPLA|20=0|150=2|60=20160916-03:47:54|10=000|'
+    $scope.postTradeData = function () {
+        var data_xhr = {
+            data_string: $scope.order_string
+        };
+        $http({
+            method: 'POST',
+            url: 'enter',
+            data: data_xhr,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(function Succes(response) {
+            console.log(response);
+            $scope.order_string = '';
+            $scope.positions = response.data;
+        }, function Error(response) {
+            console.log(response);
+            $scope.all_data = response.statusText;
+        });
     };
-    $http({
-        method: 'POST',
-        url: 'enter',
-        data: datax,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(function Succes(response) {
-        console.log(response);
-        $scope.positions = response.data;
-    }, function Error(response) {
-        console.log(response);
-        $scope.all_data = response.statusText;
-    });
 });
