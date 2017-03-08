30b484eb7a301426e9df4ec236ea185ee51c062a
cr92 <agni_18_roy@yahoo.com>
2017-03-08 11:15:22 +0530

    position shows on button click

--- public/js/index.js
+++ public/js/index.js
@@ -20,16 +20,20 @@ app.controller('index_all_positions', function ($scope, $http) {
 
 var app_x = angular.module('index_show_search', []);
 app_x.controller('index_show_submit', function ($scope, $http) {
-  $http({
-    method: 'GET',
-    url: 'instrument/12348'
-  }).then(function Succes(response) {
-    console.log(response);
-    $scope.custom_data = response.data[0];
-  }, function Error(response) {
-    console.log(response);
-    $scope.all_data = response.statusText;
-  });
+  $scope.findPosition = function () {
+    $http({
+      method: 'GET',
+      url: 'instrument/' + $scope.input_instrument_id
+    }).then(function Succes(response) {
+      console.log(response);
+      $scope.input_instrument_id = '';
+      $scope.custom_data = response.data[0];
+    }, function Error(response) {
+      console.log(response);
+      $scope.all_data = response.statusText;
+    });
+  };
+
 });
 
 angular.bootstrap(document.getElementById("show_all"), ['index_show_all_positions']);
